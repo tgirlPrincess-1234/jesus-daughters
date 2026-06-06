18d85973fb9ab52716af1a5a66775aaf679d48e6
tgirlPrincess-1234 <tgirldawodu11@gmail.com>
2026-06-07 00:11:28 +0100

    Add files via upload

--- sotw.js
+++ sotw.js
@@ -1,6 +1,22 @@
 // COMPREHENSIVE SONG OF THE WEEK SPOTLIGHT BANK
 export const comprehensiveSongBank = [
     "https://open.spotify.com/embed/track/6PjIKCyRv6DEw6n6GuVQOd?utm_source=generator",
-    "https://open.spotify.com/embed/track/1JIuXJPKUam5ac8VlShvgh?utm_source=generator"
+    "https://open.spotify.com/embed/track/1JIuXJPKUam5ac8VlShvgh?utm_source=generator",
+    "https://open.spotify.com/embed/track/0mxvkw5NNGXugvh5qVORjx?si=f2bb7aedcc2240ed",
+    "https://open.spotify.com/embed/track/2EbVNVEZicKlXM0ZJl6Y8f?si=9b669ff07ab3409c",
+    "https://open.spotify.com/embed/track/7eGDM90StP3QFkcwJPaLRg?si=620f3c0062c843ed",
+    "https://open.spotify.com/embed/track/2oyOtn2qKET1Uut1rGw8H3?si=59d9e2646b934e49",
+    "https://open.spotify.com/embed/track/13RVcx3LuemkPfIOPkhpOh?si=b494b50bc5e64968",
+    "https://open.spotify.com/embed/track/5c6R3wEtrTR1qfVmFMqjcH?si=2fbe3f96886f4c66",
+    "https://open.spotify.com/embed/track/6mRriuIHR1d4P20pn8Ekh7?si=3ddee2e6915949cd",
+    "https://open.spotify.com/embed/track/5tnIRF0Vd5lNHqtFw1saJC?si=d3dc4ddcaaba46ed",
+    "https://open.spotify.com/embed/track/5TJlT1FibWorDSMsQKYQEi?si=aec61df7ec074c7a",
+    "https://open.spotify.com/embed/track/0EWnPlQCVV2fTp0EGvLnjY?si=7a7ec6735e6e4e88",
+    "https://open.spotify.com/embed/track/7pW8qktBmU1SLIDeiVb8PP?si=8917bca50200463c",
+    "https://open.spotify.com/embed/track/1VEagF1oSqLgm5Y6lH32mS?si=fcb269a9d6e84f3c",
+    "https://open.spotify.com/embed/track/6KY6K2UVCcYHkQyI4ePMSx?si=efdc73529cd340e0",
+    "https://open.spotify.com/embed/track/5YgdFIuXiyhKdkrjuZlyRa?si=e67250664ec84f5c",
+    "https://open.spotify.com/embed/track/2By98SEaNLeoeH9yx6pEHd?si=f644cd2ccae44157",
+    "https://open.spotify.com/embed/track/6DCMLpRuQlmtShi9EB2Uv0?si=d1dcf9a4479b4cb6"
     // Just copy-paste more track embed URLs here whenever you find fire music!
 ];
